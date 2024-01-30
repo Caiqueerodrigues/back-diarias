3f7d057c94654dd2e1616ecf5159db94bfbd1460
Caíque Rodrigues <caique.almeida@extreme.digital>
2024-01-30 14:48:47 -0300

    ajuste na porta

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const app = require('./app');
 
-const port = process.env.port;
+const port = process.env.port || 3003;
 
 app.listen(port, () => {
     console.log('rodando backend');
