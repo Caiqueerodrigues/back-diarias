e509a6d3dd0d21734a3151704060b56a68e78456
Caíque Rodrigues <caique.almeida@extreme.digital>
2024-01-30 23:12:24 -0300

    ajuste a data do historico

--- src/models/paymentsModel.js
+++ src/models/paymentsModel.js
@@ -3,9 +3,10 @@ const  pool  = require('./connection');
 const getPayments = async () => {
     try {
         const result = await pool.query('SELECT * FROM pagamentos');
-        const arrayDePagamentos = result.rows;
-        arrayDePagamentos.forEach(pagamento => pagamento.date_pagamento = new Date(pagamento.date_pagamento).toLocaleDateString())
-        
+        const arrayDePagamentos = result.rows.map(pagamento => {
+            return { ...pagamento, date_pagamento: new Date(pagamento.date_pagamento).toLocaleDateString() };
+        });
+
         return arrayDePagamentos;
     } catch (error) {
         console.error('Erro ao obter pagamentos:', error.message);
